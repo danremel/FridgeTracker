f49addb49cf65997d8c49a00c02fdadeec42c6df
danremel <remel.dan@gmail.com>
2017-08-24 22:23:10 -0400

    Completed option F. Can now delete food

--- lib/fridge_tracker.rb
+++ lib/fridge_tracker.rb
@@ -5,9 +5,9 @@ puts "Enter an option:"
 puts "A) List all Fridges"
 puts "B) Add a Fridge"
 puts "C) Delete a Fridge"
-# puts "D) View all food in a fridge"
-# puts "E) Add a food item to a fridge"
-# puts "F) Eat a food item from a fridge (delete it)"
+puts "D) View all food in a fridge"
+puts "E) Add a food item to a fridge"
+puts "F) Eat a food item from a fridge (delete it)"
 # puts "G) View all drink items in a specific fridge"
 # puts "H) Add a drink item to a fridge"
 # puts "I) Consume PART of a drink from a fridge (update its size in ounces)"
@@ -51,5 +51,49 @@ if option.upcase == 'C'
     end
 end
 
-        
+# Option D
+if option.upcase == 'D'
+    puts "Showing all food:"
+    Food.all.map do |food|
+        puts "Name: " + food.name.to_s
+        puts "Weight: " + food.weight.to_s + " lbs"
+        if food.is_vegan == true
+            puts "Vegan: Yes"
+        else puts "Vegan: No"
+        end
+        # puts "Added to the fridge at: " + food.timestamps.to_s
+        puts "_ _ _ _ _ _"
+    end
+end
+
+# Option E
+if option.upcase == 'E'
+    puts "What fridge are you putting food into?"
+    Food_fridge_id = gets.chomp
+    puts "What are you putting into the fridge?"
+    Food_name = gets.chomp
+    puts "How much does the food weigh? (lbs)"
+    Food_weight = gets.chomp
+    puts "Is it vegan? Y/N"
+    if gets.chomp == "Y"
+        Food_is_vegan = true
+    else Food_is_vegan = false
+    end
+    Food.create do |food|
+        food.fridge_id = Food_fridge_id
+        food.name = Food_name
+        food.weight = Food_weight
+        food.is_vegan = Food_is_vegan
+    end
+end
+
+#Option F
+if option.upcase == 'F'
+    puts "What do you want to take out of the fridge?"
+    Food_name = gets.chomp
+    puts "Is that what you want to eat? Y/N"
+    if gets.chomp == 'Y'
+        Food.find_by_name(Food_name).destroy
+    end
+end
 
