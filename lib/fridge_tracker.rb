d794e2c5511488d6bd25d2edda32b8ab87a4981d
danremel <remel.dan@gmail.com>
2017-08-24 21:49:30 -0400

    Completed option C, can delete by Fridge ID

--- lib/fridge_tracker.rb
+++ lib/fridge_tracker.rb
@@ -4,7 +4,7 @@ require_relative '../app/models/fridge'
 puts "Enter an option:"
 puts "A) List all Fridges"
 puts "B) Add a Fridge"
-# puts "C) Delete a Fridge"
+puts "C) Delete a Fridge"
 # puts "D) View all food in a fridge"
 # puts "E) Add a food item to a fridge"
 # puts "F) Eat a food item from a fridge (delete it)"
@@ -17,7 +17,6 @@ puts "You selected #{option}"
 
 # Option A
 if option.upcase == 'A'
-    puts Fridge.all.to_s
     Fridge.all.map do |fridge|
         puts "Fridge ID = " + fridge.id.to_s
         puts fridge.location.to_s
@@ -28,5 +27,29 @@ if option.upcase == 'A'
 end
 
 # Option B
+if option.upcase == 'B'
+    puts "Where are you putting your fridge?"
+    Fridge_location = gets.chomp
+    puts "What brand is your fridge?"
+    Fridge_brand = gets.chomp
+    puts "How big is your fridge? (Cubic feet)"
+    Fridge_size = gets.chomp
+    Fridge.create do |fridge|
+        fridge.location = Fridge_location
+        fridge.brand = Fridge_brand
+        fridge.size = Fridge_size
+    end
+end
 
+# Option C
+if option.upcase == 'C'
+    puts "Select Fridge ID"
+    Fridge_id = gets.chomp
+    puts "Delete fridge " + Fridge_id + "? Y/N"
+    if gets.chomp == 'Y'
+        Fridge.find_by_id(Fridge_id).destroy
+    end
+end
+
+        
 
